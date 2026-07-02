116d4f5c1477dc4435f8d73bd6ea85f70b875cd2
Diego Fondevilla <fondevilla@chapman.edu>
2026-07-03 00:24:04 +0800

    Updated right API URL.

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 import streamlit as st
-import pandas as pd
-import requests
 
 st.set_page_config(page_title = "Schmid College Student Analysis", layout = "wide")
 
@@ -10,10 +8,3 @@ model_page = st.Page("pages/model.py", title = "PAM Model", icon = "📲", defau
 
 pg = st.navigation([home_page, dashboard_page, model_page])
 pg.run()
-
-# st.title("Schmid Student Segmentation Tool")
-# st.markdown("User interfact for viewing dashboards and clustering students")
-
-# API_URL =  "http://0.0.0.0:10000"
-
-# st.sidebar.header("Navigation")
--- pages/model.py
+++ pages/model.py
@@ -5,7 +5,7 @@ import requests
 st.title("PAM Clustering Model")
 st.markdown("Here is the page dedicated to clustering students based on the inputted dataset!")
 
-API_URL =  "http://0.0.0.0:10000" # API URL for API being hosted in Render
+API_URL =  "https://schmid-student-segmentation-api.onrender.com" # API URL for API being hosted in Render
 
 st.subheader("📂 Upload your dataset here: ")
 st.file_uploader("Upload a CSV file", type = ["csv"])
