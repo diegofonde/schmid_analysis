855f9bb54e0c292a50d4d1ca7844106cdeb5495f
Diego Fondevilla <fondevilla@chapman.edu>
2026-07-02 22:51:18 +0800

    Created initial front end using Streamlit with 3 pages: home.py, dashboard.py and model.py

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import pickle
-import gower
+import requests
 
+st.set_page_config(page_title = "Schmid College Student Analysis", layout = "wide")
+
+home_page = st.Page("pages/home.py", title = "Home", icon = "🏠", default = True)
+dashboard_page = st.Page("pages/dashboard.py", title = "Interactive Dashboard", icon = "📊", default = False)
+model_page = st.Page("pages/model.py", title = "PAM Model", icon = "📲", default = False)
+
+pg = st.navigation([home_page, dashboard_page, model_page])
+pg.run()
+
+# st.title("Schmid Student Segmentation Tool")
+# st.markdown("User interfact for viewing dashboards and clustering students")
+
+# API_URL =  "http://0.0.0.0:10000"
+
+# st.sidebar.header("Navigation")
---
+++ pages/home.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("Home")
+st.header("Welcome to the home page!")
+
+st.markdown("This webapp is dedicated to the analysis of survey data using an interactive Tableau Dashboard and a PAM cluster model!")
