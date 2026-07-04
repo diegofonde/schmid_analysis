2e65e3a384fdd4f61fc6fff7d389e0251b29f987
Diego Fondevilla <fondevilla@chapman.edu>
2026-07-04 15:00:54 +0800

    Edited Homepage

--- pages/home.py
+++ pages/home.py
@@ -3,4 +3,13 @@ import streamlit as st
 st.title("Home")
 st.header("Welcome to the home page!")
 
-st.markdown("This webapp is dedicated to the analysis of survey data using an interactive Tableau Dashboard and a PAM cluster model!")
+st.markdown("This webapp was created with the goal of long term analysis for schmid survey data!" \
+"" \
+"Orginally my role as a Student Assistant started off simple data cleaning and analysis using excel. However, as I continued with the role I wanted to find more ways to utilize my skills and increase productivity leading to the culmination of this project!" \
+"" \
+"" \
+"Feature includes:" \
+"1. Page Dedicated for Dashboard" \
+"2. Clustering Algorithm" \
+"" \
+"All features are created by Diego Fondevilla under Kate Hills supervision :))")
