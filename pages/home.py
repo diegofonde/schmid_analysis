5d5512ef808b27b3a9658f4e18a3eb666019d38f
Diego Fondevilla <fondevilla@chapman.edu>
2026-07-09 22:31:14 +0800

    Added text to the home page.

--- pages/home.py
+++ pages/home.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Home")
+st.title("Home🏠")
 st.header("Welcome to the home page!")
 
 st.markdown("""
@@ -9,10 +9,11 @@ This webapp was created with the goal of long term analysis for schmid survey da
 Orginally my role as a Student Assistant started off simple data cleaning and analysis using excel. However, as I continued with the role I wanted to find more ways to utilize my skills and increase productivity leading to the culmination of this project!
 
 ### Features include:
-1. **Page Dedicated for Dashboard:** View macro-level metrics and trends instantly.
-2. **Clustering Algorithm:** Access the machine learning pipeline to segment profiles.
+1. **📊Page Dedicated for Dashboard:** View macro-level metrics and trends instantly.
+2. **📲Clustering Algorithm:** Access the machine learning pipeline to segment profiles.
             
 This webapp is still a WIP, and developments are ongoing.
 
-_All features are created by Diego Fondevilla under Kate Hill's supervision :))_
+_All features are created by Diego Fondevilla under Kate Hill's supervision :))
 """)
+
