252113ed55b06a1cc4b18abd5c2b55636a86958a
Diego Fondevilla <fondevilla@chapman.edu>
2026-07-24 11:27:57 +0800

    Added page for database that is connected to home

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ st.set_page_config(page_title = "Schmid College Student Analysis", layout = "wid
 home_page = st.Page("pages/home.py", title = "Home", icon = "🏠", default = True)
 dashboard_page = st.Page("pages/dashboard.py", title = "Interactive Dashboard", icon = "📊", default = False)
 model_page = st.Page("pages/model.py", title = "PAM Model", icon = "📲", default = False)
+database_page = st.Page("pages/database.py", title = "Database", icon = "💾", default = False)
 
 pg = st.navigation([home_page, dashboard_page, model_page])
 pg.run()
