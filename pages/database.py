a36201d03c348c7e2d194cb51124c492e07f00a1
Diego Fondevilla <fondevilla@chapman.edu>
2026-07-27 18:02:50 +0800

    Add initital description for database page

---
+++ pages/database.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Database page 💾")
+
+st.markdown("""
+Welcome to the database page!
+
+This system uses **SQLite** to efficiently store, structure, and manage survey responses exported from **Qualtrics**. 
+
+**Key Objectives:**
+* **Scalability:** Easily ingest and organize future survey runs within a unified schema.
+* **Tableau Ready:** Automatically format and standardize dynamic survey responses for seamless visualization and reporting.
+""")
