97da8da005b83f2b0fcaf3dddad97909ba79d6c4
Diego Fondevilla <fondevilla@chapman.edu>
2026-07-31 16:06:41 +0800

    Testing png display of SQLite database schema

--- pages/database.py
+++ pages/database.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 # Gaining access to the documents folder
 ROOT = Path(__file__).parents[1]
-schema_pdf_path = ROOT/"files"/"PDF"/"Schmid Survey.pdf"
+schema_png_path = ROOT/"files"/"PNG"/"Schmid Survey (1).png"
 
 st.title("Database page 💾")
 
@@ -21,11 +21,11 @@ This system uses **SQLite** to efficiently store, structure, and manage survey r
 
 st.subheader("🔀 Database Schema")
 
-if schema_pdf_path.is_file():
-    with open(schema_pdf_path, "rb") as f:
-        base64_pdf = base64.b64encode(f.read()).decode("utf-8")
-
-    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="800" type="application/pdf"></iframe>'
-    st.markdown(pdf_display, unsafe_allow_html = True)
+if schema_png_path.is_file():
+    st.image(
+        str(schema_png_path),
+        caption = "SQLite database schema for storing of qualtrics survey data",
+        use_container_width = True
+    )
 else:
-    st.error(f"Could not find PDF file at {schema_pdf_path}")
+    st.error(f"Could not find PDF file at {schema_png_path}")
