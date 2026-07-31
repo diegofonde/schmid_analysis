a3473d69b816759fd7196487f592c7035a018e8e
Diego Fondevilla <fondevilla@chapman.edu>
2026-07-31 15:52:21 +0800

    Testing to see if pdf file for database schema will show in database.py

--- pages/database.py
+++ pages/database.py
@@ -1,5 +1,10 @@
 import streamlit as st
 import pandas as pd
+from pathlib import Path
+
+# Gaining access to the documents folder
+ROOT = Path(__file__).parents[1]
+schema_pdf_path = ROOT/"files"/"PDF"/"Scmid Survey.pdf"
 
 st.title("Database page 💾")
 
@@ -14,4 +19,8 @@ This system uses **SQLite** to efficiently store, structure, and manage survey r
 """)
 
 st.subheader("🔀 Database Schema")
-st.pdf()
+
+if schema_pdf_path.is_file():
+    st.pdf(schema_pdf_path)
+else:
+    st.error(f"Could not find PDF file at {schema_pdf_path}")
