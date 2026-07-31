fc732f0d91acd03aeb357184958bb4bd96dd7280
Diego Fondevilla <fondevilla@chapman.edu>
2026-07-31 16:31:17 +0800

    Added git ignore so that database and other sensitive information isn't uploaded to Githhub

--- pages/database.py
+++ pages/database.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import pandas as pd
-import base64
+import sqlite3
 from pathlib import Path
+
+# Connecting to SQLite database
 
 # Gaining access to the documents folder
 ROOT = Path(__file__).parents[1]
