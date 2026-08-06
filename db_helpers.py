bb24e1cff2114def83da4d07e88dde714699ea14
Diego Fondevilla <fondevilla@chapman.edu>
2026-08-06 21:28:25 +0800

    Adding function that connects to supabase

--- db_helpers.py
+++ db_helpers.py
@@ -1,10 +1,18 @@
-import sqlite3
-import tempfile
+import streamlit as st
+import pandas as pd
+from supabase import create_client, Client
 
-def get_connection(uploaded_file):
-    with tempfile.NamedTemporaryFile(delete = False, suffix = ".db") as tmp_file: # This is important since st.file_uploader() stores memory in the RAM, but in order to connect sqlite3 needs a file path
-            tmp_file.write(uploaded_file.getvalue())
-            temp_db_path = tmp_file.name
+@st.cache_resource
+def get_supabase_client() -> Client:
     
-    conn = sqlite3.connect(temp_db_path)
+    # Initializes and caches the Supabase client using Streamlit secrets.
+
+    if "supabase" not in st.secrets:
+        raise KeyError("Missing [supabase] configuration in st.secrets.")
+    
+    url = st.secrets["supabase"]["url"]
+    key = st.secrets["supabase"]["key"]
+    return create_client(url, key)
+
+
 
