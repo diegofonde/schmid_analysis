3aa2897117d49b1f449b22d79f62cf083b25a5f7
Diego Fondevilla <fondevilla@chapman.edu>
2026-08-08 15:56:56 +0800

    Adding test page for testing purposes

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ home_page = st.Page("pages/home.py", title = "Home", icon = "🏠", default = Tr
 dashboard_page = st.Page("pages/dashboard.py", title = "Interactive Dashboard", icon = "📊", default = False)
 model_page = st.Page("pages/model.py", title = "PAM Model", icon = "📲", default = False)
 database_page = st.Page("pages/database.py", title = "Database", icon = "💾", default = False)
+test_page = st.Page("pages/test.py", title = "Test", default = False )
 
 pg = st.navigation([home_page, dashboard_page, model_page, database_page])
 pg.run()
