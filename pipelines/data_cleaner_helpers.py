c77d0c9be834cee01d4ecd15a764b4fa4eab1a2f
Diego Fondevilla <fondevilla@chapman.edu>
2026-09-09 10:53:04 +0800

    Created pipeline for getting uncleaned variables

--- pipelines/data_cleaner_helpers.py
+++ pipelines/data_cleaner_helpers.py
@@ -1,12 +1,19 @@
 import pandas as pd
 import numpy as np
 
-def clean_model_data(student_list):
+def clean_model_data(connection):
 
     commuting_question = "How many minutes is your typical commute (one way)? *this includes walking, biking, driving, or public transit time"
     working_question = "How many hours per week do you typically work?"
     credits_question = "How many credits are you enrolled in this semester?"
     labs_question = "How many lab courses are you enrolled in this semester?"
+
+    question_list = [commuting_question, working_question, credits_question, labs_question]
+
+    response = (
+        connection.table("answers")
+        .select("*")
+    )
 
 
 
--- pipelines/db_helpers.py
+++ pipelines/db_helpers.py
@@ -140,10 +140,18 @@ def insert_responses(connection, df, question_map, respondent_map, response_map)
 
 def uncleaned_answers(connection):
 
+    commuting_question = "How many minutes is your typical commute (one way)? *this includes walking, biking, driving, or public transit time"
+    working_question = "How many hours per week do you typically work?"
+    credits_question = "How many credits are you enrolled in this semester?"
+    labs_question = "How many lab courses are you enrolled in this semester?"
+    
+    question_list = [commuting_question, working_question, credits_question, labs_question]
+
     response = (
             connection.table("answers")
-            .select("answer_id, questions(question_text), answer, clean_answer" )
-            .eq("clean_answer", "")
+            .select("answer_id, answer, clean_answer, questions!inner(question_text)")
+            .in_("questions.question_text", question_list)
+            .eq("clean_text_answer", "")
             .execute()
         )
 
