885a88363f1965ae53184d59fb3a50fa1d52d418
Diego Fondevilla <fondevilla@chapman.edu>
2026-09-13 01:57:26 +0800

    querying from supabase itself

--- pages/test.py
+++ pages/test.py
@@ -13,7 +13,7 @@ clean_student_list = dc.clean_raw_data(unclean_student_list)
 submission = db.upload_cleaned_answers(conn, clean_student_list)
 st.success(f"Submitted {submission} clean answers.")
 
-model_input_df = pd.read_sql("SELECT * FROM model_input;", conn)
+model_input_df = conn.query("SELECT * FROM model_input;", ttl = 60)
 
 st.dataframe(model_input_df)
 
