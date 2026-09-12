20f59ae4051ac5c2b6e2521e281cae2eddfff239
Diego Fondevilla <fondevilla@chapman.edu>
2026-09-13 01:55:55 +0800

    Testing retrieving view in test.py

--- pages/test.py
+++ pages/test.py
@@ -1,13 +1,19 @@
 import streamlit as st
+import pandas as pd
 from pipelines import data_cleaner_helpers as dc
 from pipelines import db_helpers as db
+from pipelines import model_helpers as mh
 
 conn = st.session_state.supabase
 
 st.title("Testing Page")
 
-# unclean_student_list = db.get_uncleaned_answers(conn)
-# clean_student_list = dc.clean_raw_data(unclean_student_list)
-# submission = db.upload_cleaned_answers(conn, clean_student_list)
-# st.success(f"Submitted {submission} clean answers.")
+unclean_student_list = db.get_uncleaned_answers(conn)
+clean_student_list = dc.clean_raw_data(unclean_student_list)
+submission = db.upload_cleaned_answers(conn, clean_student_list)
+st.success(f"Submitted {submission} clean answers.")
+
+model_input_df = pd.read_sql("SELECT * FROM model_input;", conn)
+
+st.dataframe(model_input_df)
 
