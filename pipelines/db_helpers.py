fba3ec45f517678a1c48b7bf303d2cebe978a1b8
Diego Fondevilla <fondevilla@chapman.edu>
2026-09-13 21:00:45 +0800

    Testing automated clustering

--- pipelines/db_helpers.py
+++ pipelines/db_helpers.py
@@ -162,3 +162,10 @@ def upload_cleaned_answers(connection, clean_list):
     connection.rpc("bulk_update_clean_answers", {"payload": clean_list}).execute
 
     return len(clean_list)
+
+def upload_clusters(connection, clustered_list):
+
+    # Sends entire payload in 1 HTTP call
+    connection.rpc("upload_clusters", {"payload": clustered_list}).execute
+
+    return len(clustered_list)
