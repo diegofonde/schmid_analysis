401973f0bfcaf1419bb71556b5ccfd3b99774e0a
Diego Fondevilla <fondevilla@chapman.edu>
2026-09-13 21:23:15 +0800

    Double checking json

--- pages/test.py
+++ pages/test.py
@@ -21,6 +21,8 @@ st.dataframe(model_input_df, use_container_width=True)
 with st.spinner("Fetching data from Supabase..."):
     predictions = mh.predict(raw_json)
 
+st.json(predictions)
+
 with st.spinner("Uploading cluster intomartion..."):
     clustered_results = db.upload_clusters(predictions)
 
